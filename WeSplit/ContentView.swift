1cbc5102d5b0715a1bbb93bc51212f1dd0d2c92f
= <=>
2025-08-15 16:33:04 -0400

    UI tutorial

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form{
+            TextField("Enter your name", text: $name)
+            Text("Your name is \(name)")
+            ForEach(0..<100){
+                Text("Row \($0 + 1)")
+            }
         }
-        .padding()
     }
 }
 
